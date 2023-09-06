654000ac5dccd1323b28876a9855876d68a36007
Bikalpa <bikalpapaudel53@gmail.com>
2023-09-06 19:01:30 +0545

    creating home page with all the icons and changing hero image to a gif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,56 @@
+import {PiStarFourFill} from 'react-icons/pi'
+import {GoDownload, GoTelescope} from 'react-icons/go'
+import {BiMessageRoundedDots} from 'react-icons/bi'
+import {HiOutlineArrowTrendingDown} from 'react-icons/hi2'
+import {FiGithub,FiLinkedin, FiTwitter} from 'react-icons/fi'
+
+
+
+
+
 import style from './page.module.css'
 export default function Home() {
   return (
     <section className={style.home}>
-      <div className={style.left}></div>
-      <div className={style.right}></div>
+      <div className={style.left}>
+        <div className={style.top}>
+          <PiStarFourFill className={style.star_icon}/>
+          <div className={style.social}>
+              <FiTwitter className={style.social_icon}/>
+              <FiGithub className={style.social_icon}/>
+              <FiLinkedin className={style.social_icon}/>
+          </div>
+        </div>
+        <div className={style.welcome}>
+          Frontend Developer
+        </div>
+        <div className={style.intro}>
+          <p className={style.intro_para}>HELLO, I AM A</p>
+          <p className={style.intro_para}>FRONTEND DEVELOPER</p>
+          <p className={style.intro_para}>FROM NEPAL</p>
+        </div>
+        <div className={style.button}>
+          <div className={style.cv}>
+            Download CV <GoDownload className={style.download_icon}/>
+          </div>
+          <div className={style.contact}>
+            Reach me here <BiMessageRoundedDots />
+          </div>
+        </div>
+        <p className={style.info}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae error molestias ut fugit eveniet animi eos asperiores quidem temporibus iure?
+        </p>
+        <div className={style.explore}>
+          <div className={style.green}></div>
+          <div className={style.explore_text}>Want to learn more about me?</div>
+        </div>
+        <div className={style.explore_part}>
+          <HiOutlineArrowTrendingDown className={style.arrow}/>
+          <div className={style.explore_btn}>EXPLORE <GoTelescope /></div>
+        </div>
+      </div>
+      <div className={style.right}>
+      </div>
     </section>
   )
 }
