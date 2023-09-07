e00965e673fa9ceee51d7131bbcaf0d069a6149f
Bikalpa <bikalpapaudel53@gmail.com>
2023-09-07 11:28:13 +0545

    did frontend design

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,8 +1,18 @@
+import {AiOutlineMail} from 'react-icons/ai'
+import {FiLinkedin, FiTwitter} from 'react-icons/fi'
 import style from './styles.module.css'
 export default function Footer() {
   return (
     <footer className={style.footer}>
-        hello
+        <div className={style.email}><AiOutlineMail /> bikalpapaudel53@gmail.com</div>
+        <div className={style.copywrite}>&copy; 2023 | Bikalpa Paudel </div>
+        <div className={style.connect}>
+          <div className={style.icon}>
+            <FiLinkedin />
+            <FiTwitter />
+          </div>
+          <p className={style.connect_text}>connect with me</p>
+        </div>
     </footer>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,8 +37,8 @@ export default function Home() {
             Reach me here <BiMessageRoundedDots />
           </div>
         </div>
-        <p className={style.info}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae error molestias ut fugit eveniet animi eos asperiores quidem temporibus iure?
+        <p className={style.info}>      
+          I'm currently focused on honing my frontend development skills and have a strong desire to expand my expertise into backend and other areas of software development in the future.
         </p>
         <div className={style.explore}>
           <div className={style.green}></div>
