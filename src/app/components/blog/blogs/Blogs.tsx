b4eef74f6fe0ef6a125daa639809fff99b18eff9
Bikalpa <bikalpapaudel53@gmail.com>
2023-11-07 13:39:26 +0545

    error handling

--- src/app/components/blog/blogs/Blogs.tsx
+++ src/app/components/blog/blogs/Blogs.tsx
@@ -1,29 +1,34 @@
 import style from "./style.module.css";
 async function Blogs() {
   const res = await getAllBlog();
-  console.log(res)
-  const allBlogs = res.posts;
+  console.log(res);
+  const allBlogs = res?.posts;
 
-  const blogPosts = allBlogs?.map((item: any) => {
-    return (
-      <div key={item.id} className={style.blog_post}>
-        <div className={style.blog_image}></div>
-
-        <div className={style.blog_info}>
-          <h1>{item.title}</h1>
-          <p>Created : {item.date}</p>
+  return (
+    <div className={style.blogs}>
+  {allBlogs
+    ? allBlogs.map((item: any) => (
+        <div key={item.id} className={style.blog_post}>
+          <div className={style.blog_image}></div>
+          <div className={style.blog_info}>
+            <h1>{item.title}</h1>
+            <p>Created : {item.date}</p>
+          </div>
         </div>
-      </div>
-    );
-  });
-  return <div className={style.blogs}>{blogPosts}</div>;
+      ))
+    : <div>No Blogs</div>
+  }
+    </div>
+  );
 }
-
 
 export async function getAllBlog() {
   const URL = process.env.NEXT_PUBLIC_URL;
-    const res = await fetch(`https://bikalpa-paudel.vercel.app/api/blogs`, { cache: 'no-store' } );
-    const json = await res.json(); 
-    return json;
+  const res = await fetch(`${URL}/api/blogs`, { cache: "no-store" });
+  if(!res.ok){
+    return "error"
+  }
+  const json = await res.json();
+  return json;
 }
 export default Blogs;
