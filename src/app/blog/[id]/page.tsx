81bd2755e1e538675027fcf287b7e789083633b9
Bikalpa <bikalpapaudel53@gmail.com>
2023-11-09 08:57:14 +0545

    small changes in individual blog section

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import style from "./blog.module.css"
 async function page({ params }: { params: { id: string } }) {
-const URL = process.env.NEXT_PUBLIC_URL;
+  const URL = process.env.NEXT_PUBLIC_URL;
   const blogPost = await fetch(`${URL}/api/blogs/${params.id}`);
   const blog = await blogPost.json();
+  console.log(blog)
   if (blog) {
-    return <div>hellp</div>;
+    return (
+      <section className={style.blog_section}>
+        <div className={style.blog}>
+          <h1>{blog.post.title}</h1>
+          <p>{blog.post.date}</p>
+          <p>{blog.post.description}</p>
+        </div>
+      </section>
+    );
   } else {
     return <div>nothing</div>;
   }
