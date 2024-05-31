ae99f7341668e03725577062d6144908f41fbefd
Bikalpa-Paudel <bikalpapaudel53@gmail.com>
2024-05-31 11:38:21 +0545

    half of hero and about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,6 +11,7 @@ import tailwind from "@/public/Images/tailwind.png";
 import typescript from "@/public/Images/typescript.png";
 import Image from "next/image";
 import { Metadata } from "next";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const metadata: Metadata = {
   title: "About",
@@ -65,9 +66,7 @@ function About() {
         <div className="flex items-center gap-[150px]">
           <PiStarFourFill className="text-[3rem] text-[#cccccc]" />
           <div className="relative">
-            <h1 className="text-2xl relative z-10">
-              Know about me
-            </h1>
+            <h1 className="text-2xl relative z-10">Know about me</h1>
             <div className="absolute top-3 left-2 w-full h-full bg-gray-300 z-[-98]"></div>
           </div>
         </div>
@@ -101,7 +100,74 @@ function About() {
           </div>
         </div>
       </div>
-      <div className="flex-[1] bg-[#222222]">
+      <div className="flex-[1] bg-[#222222] border border-black">
+        <Tabs defaultValue="experience" className="w-full p-4">
+          <TabsList className="w-full p-0">
+            <TabsTrigger
+              value="experience"
+              className="w-1/2 border border-[#fff]"
+            >
+              Experience
+            </TabsTrigger>
+            <TabsTrigger
+              value="education"
+              className="w-1/2 border border-[#fff]"
+            >
+              Education
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="experience" className="text-white">
+            {/* make tree like structure to point listing experince */}
+            <ul className="flex flex-col gap-5">
+              <li className="">
+                <h3 className="flex items-center gap-2">
+                  <PiStarFourFill /> Void Nepal Pvt. Ltd.{" "}
+                  <span>(september 2023 -- february 2024)</span>
+                </h3>
+                <p></p>
+              </li>
+              <li className="">
+                <h3 className="flex items-center gap-2">
+                  <PiStarFourFill />College's Development Platform{" "}
+                  <span>(september 2023 -- february 2024)</span>
+                </h3>
+                <p></p>
+              </li>
+              <li className="">
+                <h3 className="flex items-center gap-2">
+                  <PiStarFourFill /> Freelancing{" "}
+                  <span>(september 2023 -- february 2024)</span>
+                </h3>
+                <p></p>
+              </li>
+            </ul>
+          </TabsContent>
+          <TabsContent value="education" className="text-[#fff]">
+          <ul className="flex flex-col gap-5">
+              <li className="">
+                <h3 className="flex items-center gap-2">
+                  <PiStarFourFill /> Herald Collage Kathmandu{" "}
+                  <span>(Undergraduate)</span>
+                </h3>
+                <p></p>
+              </li>
+              <li className="">
+                <h3 className="flex items-center gap-2">
+                  <PiStarFourFill />Kalika Manavgyan School{" "}
+                  <span>(High School)</span>
+                </h3>
+                <p></p>
+              </li>
+              <li className="">
+                <h3 className="flex items-center gap-2">
+                  <PiStarFourFill />Self Taught{" "}
+                  <span>(Forever)</span>
+                </h3>
+                <p></p>
+              </li>
+            </ul>
+          </TabsContent>
+        </Tabs>
       </div>
     </section>
   );
